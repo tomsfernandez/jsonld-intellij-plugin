7c20f0fbd03188049968c2c2ead5488131d6aecb
GitHub Action <action@github.com>
2021-09-23 18:16:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tomsfernandez/jsonldintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tomsfernandez.jsonldintellijplugin.services
+
+import com.github.tomsfernandez.jsonldintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
